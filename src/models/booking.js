d64e700a8114001b37763f3be45397d1c311b557
Dishu <dishuraj129@gmail.com>
2024-03-01 15:41:21 +0530

    Added migration to add new column in booking table

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,36 +1,44 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require('sequelize'); 
+
+module.exports = (sequelize) => {
   class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // Define associations here
     }
   }
-  Booking.init({
-    flightId: {
-      type: Sequelize.INTEGER,
-      allowNull:false,
+  Booking.init(
+    {
+      flightId: {
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        allowNull:false,
+        values:['Inprocess','Booked','Cancelled'],
+        defaultValue:'Inprocess'
+      },
+      noOfSeats:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:1
+      },
+      totalCost:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:0
+      }
+      
     },
-    userId: {
-      type: Sequelize.INTEGER,
-      allowNull:false,
-    },
-    status: {
-      type: Sequelize.ENUM,
-      allowNull:false,
-      values:['Inprocess','Booked','Cancelled'],
-      defaultValue:'Inprocess'
+    {
+      sequelize,
+      modelName: 'Booking',
     }
-  }, {
-    sequelize,
-    modelName: 'Booking',
-  });
+  );
   return Booking;
 };
