76a9c56113255fa5ad1b66f0dd2ca694cf2b2218
Dishu <dishuraj129@gmail.com>
2024-03-01 13:08:02 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+const {PORT}=require('./config/serverConfig')
+const apiRoutes=require('./routes/index')
+const db=require('./models/index')
+const setupAndStartServer=()=>{
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
+    app.use('/api',apiRoutes)
+
+    app.listen(PORT,()=>{
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true})
+        }
+        console.log(`Server started on Port ${PORT}`)
+    })
+}
+
+setupAndStartServer()
